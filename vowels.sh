fac0aa6f350469d3da4e382a2bbb354f355bcf19
Anjaliev <133093479+Anjaliev@users.noreply.github.com>
2024-03-01 15:16:18 +0530

    Add files via upload

---
+++ vowels.sh
@@ -0,0 +1,6 @@
+echo "Anjali E V(23MCA016)"
+echo  "Enter a line of text: "
+read text
+text_lower=$(echo "$text" | tr '[:upper:]' '[:lower:]')
+count=$(echo "$text_lower" | tr -cd 'aeiou' | wc -c)
+echo "Number of vowels in the text: $count"
