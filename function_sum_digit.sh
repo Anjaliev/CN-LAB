bf2aacef0a038dafb1ea9b40ee1a26aa719cb558
Anjaliev <133093479+Anjaliev@users.noreply.github.com>
2024-03-29 23:59:35 +0530

    Add files via upload

---
+++ function_sum_digit.sh
@@ -0,0 +1,17 @@
+echo "Anjali E V(23MCA016)"
+sum_of_digits() 
+{
+num=$1
+sum=0
+while [ $num -gt 0 ]; 
+do
+sum=$((sum + num % 10)) 
+num=$((num / 10))       
+done
+echo $sum
+}
+echo "Enter a number:"
+read number
+result=$(sum_of_digits $number)
+echo "Sum of digits of $number is $result"
+
