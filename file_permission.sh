74ea15aefdbd3662b42a07b0a1ce65b8977cca8a
Anjaliev <133093479+Anjaliev@users.noreply.github.com>
2024-03-30 13:09:43 +0530

    Add files via upload

---
+++ file_permission.sh
@@ -0,0 +1,15 @@
+echo "Anjali E V(23MCA016)"
+for file in *; 
+do
+if [ -f "$file" ]; 
+then
+if [ ! -x "$file" ]; 
+then
+echo "Adding execute permission to $file"
+chmod +x "$file"
+else
+echo "$file already has execute permissions."
+fi
+fi
+done
+
